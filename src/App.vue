72e4e1c36deae5c3db3ed6fd433723d5f985ec2e
Rosie Liu <rosieliu@usc.edu>
2019-10-22 10:39:41 +0000

    done with cards

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,24 @@
         <b-container class="bv-example-row">
         <b-row>
         <b-col cols="12">
-
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg ">
-            <button class="btn btn-outline-dark" type="button">
-                <router-link to="/" > Home </router-link>
-            </button>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <button class="btn btn-outline-dark" type="button">
-                        <router-link to="/menu" > Menu </router-link>
-                        </button>
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btn-outline-dark" type="button">
-                        <router-link to="/about" > About </router-link>
-                        </button>
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btn-outline-dark" type="button">
-                        <router-link to="/contact" > Contact </router-link>
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-link to="/"  router-tag="b-navbar-brand">Home</b-link>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-link to="/menu"  router-tag="b-nav-item">Menu</b-link>
+                    <b-link to="/about"  router-tag="b-nav-item">About</b-link>
+                    <b-link to="/contact"  router-tag="b-nav-item">Contact</b-link>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        </div>
         <RouterView />
 
         </b-col>
         </b-row>
         </b-container>
-
 
     </div>
 </template>
